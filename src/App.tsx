04a1bb02610959cda19e0393f56fbd1f3a97934b
Carri-ann Bloom <carriannbloom@Carri-anns-MacBook-Air.local>
2025-02-24 14:07:10 +0200

    add filter bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "./components/theme-provider"
 import { RevenueBreakdown } from "./pages/revenue-breakdown"
 import { Header } from "./components/header"
 import { NationalOverview } from "./pages/national-overview"
+import { FilterBar } from "./components/filter-bar"
 
 function App() {
+  const [selectedYears, setSelectedYears] = useState(10)
+  const [selectedTopN, setSelectedTopN] = useState(5)
+
+  const handleYearChange = (value: string) => {
+    setSelectedYears(Number(value))
+  }
+  const handleTopNChange = (value: string) => {
+    setSelectedTopN(Number(value))
+  }
+
   return (
     <>
       <BrowserRouter>
@@ -13,8 +25,9 @@ function App() {
             <div data-wrapper="" className="border-grid flex flex-1 flex-col">
               <Header />
 
-              <main className="flex flex-1 flex-col">
-                <div className="container-wrapper flex flex-1">
+              <main className="">
+                <div className="container-wrapper">
+                  <FilterBar selectedYears={selectedYears} selectedTopN={selectedTopN} onYearChange={handleYearChange} onTopNChange={handleTopNChange} />
                   <Routes>
                     <Route path="/" element={<NationalOverview />} />
                     <Route path="/national-revenue-breakdown" element={<RevenueBreakdown />} />
---
+++ src/components/filter-bar.tsx
@@ -0,0 +1,60 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
+type FilterBarProps = {
+  selectedYears: number,
+  selectedTopN: number,
+  onYearChange: (value: string) => void
+  onTopNChange: (value: string) => void
+}
+
+export const FilterBar = ({
+  selectedYears,
+  selectedTopN,
+  onYearChange,
+  onTopNChange
+}: FilterBarProps) => {
+  const yearOptions = [1, 5, 10, 15]
+  const topNOptions = [5, 10, -1]
+  return (
+    <div className="px-6 py-3">
+      <Card>
+        <CardContent>
+          <div className="w-full bd-white">
+            <div className="max-w-6xl flex items-center gap-4">
+              <div className="flex items-center gap-2">
+                <span className="text-sm font-medium">Last</span>
+                <Select value={selectedYears.toString()} onValueChange={onYearChange}>
+                  <SelectTrigger className="w-50">
+                    <SelectValue placeholder="Select years" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {yearOptions.map(year => (
+                      <SelectItem key={year} value={year.toString()}>{year === 1 ? 'year' : year.toString() + ' years'}</SelectItem>
+                    ))}
+                  </SelectContent>
+
+                </Select>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="text-sm font-medium">Top</span>
+                <Select value={selectedTopN.toString()} onValueChange={onTopNChange}>
+                  <SelectTrigger className="w-50">
+                    <SelectValue placeholder="Select top" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {topNOptions.map(n => (
+                      <SelectItem key={n} value={n.toString()}> {n === -1 ? 'All' : n.toString()} </SelectItem>
+                    ))}
+                  </SelectContent>
+
+                </Select>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
